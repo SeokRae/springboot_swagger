3daaaef01299f9d11d4577bce8b960ac9a8e672b
seokrae <kslbsh@gmail.com>
2020-06-28 23:06:54 +0900

    테스트 코드 오류로 일단 저장해 놓고 다음꺼 진행

--- src/test/java/com/user/swagger/domain/user/UserRepositoryTests.java
+++ src/test/java/com/user/swagger/domain/user/UserRepositoryTests.java
@@ -1,11 +1,9 @@
 package com.user.swagger.domain.user;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -13,13 +11,15 @@ import java.util.Optional;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTests {
+
     @Autowired
     private UserRepository userJpaRepo;
+
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
+
 
     @Test
     public void whenFindByUid_thenReturnUser() {
